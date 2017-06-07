46df27c8bd905cb6d44557a625f2730c4b7342d1
davidco5 <dcorcos@gmail.com>
2017-06-07 17:45:44 +0300

    added my code folder

---
+++ MyCode/SegmentationMain.m
@@ -0,0 +1,5 @@
+cd('C:\Users\dcorc\OneDrive\TAU 2\Advanced Topics in Medical Image Processing 1\CNN_project')
+addpath(genpath('C:\Program Files\MATLAB\MatConvNet'))
+addpath(genpath(fullfile(pwd,'FCNN_Code')))
+
+
